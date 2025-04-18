92de3564b6fc68481e5809225928f91da1b7cc8e
Idam Achmad Faizin <idamachmadfaizin@gmail.com>
2025-04-18 13:30:56 +0700

    feat: add @fastify/compress.

---
+++ src/config/compress.ts
@@ -0,0 +1,3 @@
+import { FastifyCompressOptions } from "@fastify/compress";
+
+export const compressOptions: FastifyCompressOptions = {};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+import compress from "@fastify/compress";
 import cors from "@fastify/cors";
 import env from "@fastify/env";
 import helmet from "@fastify/helmet";
 import app from "./app";
+import { compressOptions } from "./config/compress";
 import { corsOptions } from "./config/cors";
 import { envOptions, Envs } from "./config/env";
 import { helmetOptions } from "./config/helmet";
@@ -24,7 +26,8 @@ const onReady = (err: Error | null): void => {
 (async () => {
   await app.register(env, envOptions);
   await app.register(cors, corsOptions);
-  app.register(helmet, helmetOptions);
+  await app.register(helmet, helmetOptions);
+  await app.register(compress, compressOptions);
 
   app.get("/", async (_, res) => {
     res.send({ hello: "world" });
